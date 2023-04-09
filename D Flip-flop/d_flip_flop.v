896c56ef48562bbcb9f83d4c52ae916175fac30c
haneena-beevi <124423041+haneena-beevi@users.noreply.github.com>
2023-04-10 02:49:01 +0530

    Create d_flip_flop.v

---
+++ D Flip-flop/d_flip_flop.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09.04.2023 00:28:13
+// Design Name: 
+// Module Name: d_flip_flop
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module d_flip_flop(q,d,clk,rst);
+
+input d,clk,rst,rst;
+output reg q;
+
+always @(posedge clk or negedge rst)
+begin
+if(!rst)
+    q<=0;
+else
+    q<=d;
+end
+
+endmodule
